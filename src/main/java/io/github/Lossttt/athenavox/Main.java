a154dabdc34af2f66c5d71922a3be9b0cb6b0c11
Lossttt <lfatmi.ayoub@gmail.com>
2023-03-09 16:38:48 +0100

    Updated stage scenes

--- src/main/java/io/github/Lossttt/athenavox/Main.java
+++ src/main/java/io/github/Lossttt/athenavox/Main.java
@@ -1,20 +1,15 @@
 package io.github.Lossttt.athenavox;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
-        stage.setScene(scene);
-        stage.show();
+        stage.setScene(SceneRegistry.LOGIN_SCENE); // sets the LOGIN_SCENE
+        stage.show(); // shows the LOGIN_SCENE
     }
     public static void main(String[] args) {
 
@@ -23,7 +18,7 @@ public class Main extends Application {
         counter.printResults();
         counter.printCandidateHistograms();
         System.out.println("------ END of STATISTICS ------");
-
+        
         //launch();
     }
 }
