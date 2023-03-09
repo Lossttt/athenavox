bacf6690bc777b48830e57e031f0fb68b7c82fdc
Lossttt <lfatmi.ayoub@gmail.com>
2023-03-09 22:20:53 +0100

    Updated files

--- src/main/java/io/github/Lossttt/athenavox/Main.java
+++ src/main/java/io/github/Lossttt/athenavox/Main.java
@@ -1,8 +1,12 @@
 package io.github.Lossttt.athenavox;
 
+import java.io.IOException;
+import java.util.Objects;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Main extends Application {
--- src/main/java/io/github/Lossttt/athenavox/SceneRegistry.java
+++ src/main/java/io/github/Lossttt/athenavox/SceneRegistry.java
@@ -9,11 +9,10 @@ public class SceneRegistry {
 
     public static Scene register(String filename) {
         try {
-            // Scene scene = new Scene(FXMLLoader.load(Main.class.getResource("/io/github/Lossttt/athenavox/scenes/" + filename + ".fxml")));
-            // scene.getStylesheets().add(Main.class.getResource("styles.css").toExternalForm());
-            // return scene;
-            return new Scene(FXMLLoader.load(Main.class.getResource(
-                "/io/github/Lossttt/athenavox/scenes/" + filename + ".fxml")));
+
+            return new Scene(FXMLLoader.load(Main.class.getResource("/io/github/Lossttt/athenavox/scenes/" + filename + ".fxml")));
+
+
         }
         catch (Exception e){
             e.printStackTrace();
