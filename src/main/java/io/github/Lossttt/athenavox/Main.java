cd7d748852ad76a4d74583fcf0cf73d8d9b22c17
Lossttt <lfatmi.ayoub@gmail.com>
2023-03-09 14:31:24 +0100

    Added javafx start function

--- src/main/java/io/github/Lossttt/athenavox/Main.java
+++ src/main/java/io/github/Lossttt/athenavox/Main.java
@@ -1,13 +1,29 @@
-package main.java;
+package io.github.Lossttt.athenavox;
 
-public class Main {
-    public static <String> void main(String[] args) {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
-        VoteCounter counter = new VoteCounter("B:\\Java\\athenavox\\athenavox\\AthenaVox\\src\\main\\java\\Vote_data.text");
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) {
+        String javaVersion = System.getProperty("java.version");
+        String javafxVersion = System.getProperty("javafx.version");
+        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+        Scene scene = new Scene(new StackPane(l), 640, 480);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public static void main(String[] args) {
+
+        VoteCounter counter = new VoteCounter("B:\\Java\\athenavox\\Vote_data.text");
         System.out.println("------ BEGIN of STATISTICS ------");
         counter.printResults();
         counter.printCandidateHistograms();
         System.out.println("------ END of STATISTICS ------");
 
+        //launch();
     }
 }
