82bdb6e001e195fb2e5466af270ed2673a3aeea4
Lossttt <lfatmi.ayoub@gmail.com>
2023-03-10 17:45:53 +0100

    Added comments to define the function of each line

--- src/main/java/io/github/Lossttt/athenavox/VoteCounter.java
+++ src/main/java/io/github/Lossttt/athenavox/VoteCounter.java
@@ -1,5 +1,7 @@
+// Package
 package io.github.Lossttt.athenavox;
 
+// Imports
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -7,37 +9,40 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class VoteCounter {
-    private static final int NUM_CANDIDATES = 8;
-    private static final int AGE_MASK = 0xFE00;
-    private static final int GENDER_MASK = 0x0100;
-    private static final int CANDIDATE_MASK = 0xFF;
-    private static final int NUM_AGE_GROUPS = 4;
+    private static final int NUM_CANDIDATES = 8;     // Constant that defines the number of candidates in the election.
+    private static final int AGE_MASK = 0xFE00;      // Constant that masks the age bits in a vote.
+    private static final int GENDER_MASK = 0x0100;   // Constant that masks the gender bit in a vote.
+    private static final int CANDIDATE_MASK = 0xFF;  // Constant the masks the candidate bit in a vote.
+    private static final int NUM_AGE_GROUPS = 4;     // Constant that defines the number of age groups in the election.
 
-    private List<Integer> validVotes;
-    private int[] candidateVotes;
-    private int[] maleVotes;
-    private int[] femaleVotes;
-    private int[] ageGroupVotes;
-    private int[] ageGroupCounts;
+    private List<Integer> validVotes;                // List of valid votes.
+    private int[] candidateVotes;                    // Array of vote counts for each candidate.
+    private int[] maleVotes;                         // Array of vote counts for male voters.
+    private int[] femaleVotes;                       // Array of vote counts for female voters.
+    private int[] ageGroupVotes;                     // Array of vote counts for each voter in age group.
+    private int[] ageGroupCounts;                    // Array of counts of voters in each age group.
 
-    public VoteCounter(String filename) {
-        validVotes = new ArrayList<>();
-        candidateVotes = new int[NUM_CANDIDATES];
-        maleVotes = new int[NUM_CANDIDATES];
-        femaleVotes = new int[NUM_CANDIDATES];
+    public VoteCounter(String filename) {            
+        validVotes = new ArrayList<>();              // Initialize list of valid votes.
+        candidateVotes = new int[NUM_CANDIDATES];    // Initialize array of vote counts for each candidate.   
+        maleVotes = new int[NUM_CANDIDATES];         // Initialize array of vote counts for each male voter.
+        femaleVotes = new int[NUM_CANDIDATES];       // Initialize array of vote counts for each frmale voter.
         ageGroupVotes = new int[NUM_AGE_GROUPS * NUM_CANDIDATES];
         ageGroupCounts = new int[NUM_AGE_GROUPS];
 
         try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
             String line;
             while ((line = reader.readLine()) != null) {
-                int vote = Integer.parseInt(line, 16);
+                int vote = Integer.parseInt(line, 16); // For each line, the code converts it from a hexadecimal string to an integer using Integer.parseInt 
                 int age = (vote & AGE_MASK) >> 9;
                 int gender = (vote & GENDER_MASK) >> 7;
                 int candidate = vote & CANDIDATE_MASK;
 
                 if (isValidVote(age, candidate)) {
                     validVotes.add(vote);
+                    // The code checks if the vote is valid by calling the isValidVote() method, 
+                    // passing in the age and candidate as arguments. 
+                    // ** If the vote is valid, it adds the vote to the validVotes. **
                 }
             }
         } catch (IOException e) {
@@ -46,6 +51,7 @@ public class VoteCounter {
         }
 
         calculateStatistics();
+        // After all votes have been processed, the code call the method below to calculate statistics on the valid votes.
     }
 
     public void printCandidateHistograms() {
