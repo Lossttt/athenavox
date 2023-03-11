704ef3450e3fcb0352e036ffb2bfc3f4b143aca2
Lossttt <lfatmi.ayoub@gmail.com>
2023-03-11 22:09:46 +0100

    Added counter object to calls printToFile method

--- src/main/java/io/github/Lossttt/athenavox/Main.java
+++ src/main/java/io/github/Lossttt/athenavox/Main.java
@@ -12,18 +12,20 @@ import javafx.stage.Stage;
 public class Main extends Application {
     @Override
     public void start(Stage stage) {
-        stage.setScene(SceneRegistry.LOGIN_SCENE); // sets the LOGIN_SCENE
+        stage.setScene(SceneRegistry.WELCOME_SCENE); // sets the LOGIN_SCENE
         stage.show(); // shows the LOGIN_SCENE
     }
     public static void main(String[] args) {
 
         VoteCounter counter = new VoteCounter("B:\\Java\\athenavox\\Vote_data.text");
-        System.out.println("------ BEGIN of STATISTICS ------");
-        counter.printResults();
-        counter.printCandidateHistograms();
-        System.out.println("------ END of STATISTICS ------");
+        // System.out.println("------ BEGIN of STATISTICS ------");
+        // counter.printResults();
+        // counter.printCandidateHistograms();
+        // System.out.println("------ END of STATISTICS ------");
+
+        counter.printToFile("vote_results.txt");
         
-        launch();
+        // launch(); // Comment this if you don't want to stage the scene
     
     }
 }
