33bc19800365a0a5d85e3219ed757c88056df33f
Lossttt <lfatmi.ayoub@gmail.com>
2023-03-18 17:06:55 +0100

    Updated main

--- src/main/java/io/github/Lossttt/athenavox/Main.java
+++ src/main/java/io/github/Lossttt/athenavox/Main.java
@@ -1,12 +1,6 @@
 package io.github.Lossttt.athenavox;
 
-import java.io.IOException;
-import java.util.Objects;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -18,10 +12,9 @@ public class Main extends Application {
     public static void main(String[] args) {
 
         VoteCounter counter = new VoteCounter("B:\\Java\\athenavox\\Vote_data.text");
-        // System.out.println("------ BEGIN of STATISTICS ------");
-        // counter.printResults();
-        // counter.printCandidateHistograms();
-        // System.out.println("------ END of STATISTICS ------");
+        System.out.println("------ BEGIN of STATISTICS ------");
+        counter.printResults();
+        System.out.println("------ END of STATISTICS ------");
 
         counter.printToFile("vote_results.txt");
         
