cdaa738bb3081e6c906b70b36499f8bdf60178a1
Lossttt <lfatmi.ayoub@gmail.com>
2023-03-18 23:04:10 +0100

    Updated Main

--- src/main/java/io/github/Lossttt/athenavox/Main.java
+++ src/main/java/io/github/Lossttt/athenavox/Main.java
@@ -1,5 +1,7 @@
 package io.github.Lossttt.athenavox;
 
+import java.security.Principal;
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 
@@ -9,14 +11,13 @@ public class Main extends Application {
         stage.setScene(SceneRegistry.WELCOME_SCENE); // sets the LOGIN_SCENE
         stage.show(); // shows the LOGIN_SCENE
     }
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         VoteCounter counter = new VoteCounter("B:\\Java\\athenavox\\Vote_data.text");
         System.out.println("------ BEGIN of STATISTICS ------");
+        counter.printHistogram();
         counter.printResults();
-        System.out.println("------ END of STATISTICS ------");
-
-        counter.printToFile("vote_results.txt");
+        System.out.println("------ END of STATISTICS ------");;
         
         // launch(); // Comment this if you don't want to stage the scene
     
