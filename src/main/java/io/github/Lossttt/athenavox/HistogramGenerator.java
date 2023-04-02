13cc82ae15acb699e2a7d31abd5dc2ad3e35b615
Lossttt <lfatmi.ayoub@gmail.com>
2023-04-02 16:01:08 +0200

    Added abstract class for displaying histograms

---
+++ src/main/java/io/github/Lossttt/athenavox/HistogramGenerator.java
@@ -0,0 +1,7 @@
+package io.github.Lossttt.athenavox;
+
+public abstract class HistogramGenerator {
+    public abstract void printTotalVotesHistogram(int[] candidateVotes, String title);
+    public abstract void printMaleVotesHistogram(int[] maleVotes, String title);
+    public abstract void printFemaleVotesHistogram(int[] femaleVotes, String title);
+}
